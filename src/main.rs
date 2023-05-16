f78befd75bd88091398e5f446ad157634d4dd916
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-05-16 10:23:54 +0200

    implement queue with tail linked list

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod vector;
 mod linked_list;
 mod tail_linked_list;
+mod tail_linked_list_queue;
 
 fn main() {
     println!("Hello, world!");
--- src/tail_linked_list_queue.rs
+++ src/tail_linked_list_queue.rs
@@ -1,4 +1,57 @@
+use crate::tail_linked_list::TailLinkedList;
 
 pub struct Queue<T> {
-   data: Vec<T>, 
+   data: TailLinkedList<T>,
+}
+
+impl<T> Queue<T> {
+   pub fn new() -> Self {
+      Self {
+         data: TailLinkedList::new(),
+      }
+   }
+
+   pub fn enqueue(&mut self, value: T) {
+      self.data.push_back(value);
+   }
+
+   pub fn dequeue(&mut self) -> Option<T> {
+      self.data.pop_front()
+   }
+
+   pub fn is_empty(&self) -> bool {
+      self.data.is_empty()
+   }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+   #[test]
+   fn test_new() {
+      let queue = super::Queue::<i32>::new();
+      assert!(queue.is_empty());
+   }
+
+   #[test]
+   fn test_enqueue() {
+      let mut queue = super::Queue::<i32>::new();
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      assert!(!queue.is_empty());
+   }
+
+   #[test]
+   fn test_dequeue() {
+      let mut queue = super::Queue::<i32>::new();
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      assert_eq!(queue.dequeue(), Some(1));
+      assert_eq!(queue.dequeue(), Some(2));
+      assert_eq!(queue.dequeue(), Some(3));
+      assert_eq!(queue.dequeue(), None);
+   }
 }
